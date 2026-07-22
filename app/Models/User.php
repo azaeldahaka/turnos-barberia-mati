0d860c4e07d34ff4560387c8435f063e260e8b09
Azael Dahaka <azaeldahaka.v@gmail.com>
2026-07-22 20:15:00 -0300

    Fix: FilamentUser interface para produccion y TrustProxies

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+class User extends Authenticatable implements FilamentUser
 {
     /**
      * The attributes that are mass assignable.
@@ -46,5 +49,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Permitir acceso al panel en producción
+        return true;
     }
 }
